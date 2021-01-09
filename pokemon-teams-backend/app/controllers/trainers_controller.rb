386be6c7703ed5cbb26cf51d1ca9a626cc5cb315
Your Name <you@example.com>
2021-01-08 22:39:59 -0500

    Done.

---
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -0,0 +1,9 @@
+class TrainersController < ApplicationController
+    def index
+        trainers = Trainer.all
+        render json: trainers.to_json(
+            :include => { :pokemons => { only: [:id, :species, :nickname, :trainer_id] } },
+            :only => [:id, :name]
+        )
+    end
+end
